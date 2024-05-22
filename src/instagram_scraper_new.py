8dc07beebad08d606db87ad3e4e86d02e5918e00
abdullah.naeem <abdullah.naeem@10pearls.com>
2024-05-22 11:20:37 +0500

    Added insta user

--- src/instagram_scraper_new.py
+++ src/instagram_scraper_new.py
@@ -1,7 +1,9 @@
 import datetime
+import time
 from selenium import webdriver
 from pymongo import MongoClient
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
@@ -10,6 +12,7 @@ from logger import MyLogger
 from constants import MongoDB
 from constants import Messages
 from constants import Constants
+from constants import UserCredentials
 
 class Scraper:
     """Instagram Crawler"""
@@ -23,7 +26,7 @@ class Scraper:
     def search_instagram(self, max_page=10, max_post=1000):
         """Search Instagram Posts"""
         start_dt = datetime.datetime.now()
-        url = Constants.base_url.value + self.keyword + "/"
+        url = Constants.tag_url.value + self.keyword + "/"
         self.driver.get(url)
 
         # Wait to finish loading to get all dynamically-loaded content on webpage
@@ -124,13 +127,80 @@ class Scraper:
         self.logger.info(Messages.total_seconds.value.format(time_diff.seconds))
 
         return posts
+    
 
-    def insert_into_mongodb(self, posts):
+    def login_user(self):
+        url = Constants.login_url.value
+        self.driver.get(url)
+
+        # Wait to finish loading to get all dynamically-loaded content on webpage
+        WebDriverWait(self.driver, 5).until(
+            EC.presence_of_all_elements_located((By.TAG_NAME, "form"))
+        )
+
+        # Find the username and password input fields
+        username_field = self.driver.find_element(By.NAME, 'username')
+        password_field = self.driver.find_element(By.NAME, 'password')
+
+        # Replace with your Instagram credentials
+        username = UserCredentials.username.value
+        password = UserCredentials.password.value
+
+        # Enter the username and password
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+
+        # Submit the form
+        password_field.send_keys(Keys.RETURN)
+
+    
+    def search_user(self):
+        """Search Instagram Posts"""
+        start_dt = datetime.datetime.now()
+
+        self.login_user()
+        # Wait for a few seconds to ensure login is successful
+        time.sleep(10)
+
+        self.driver.get(f'{Constants.base_url.value}{self.keyword}')
+
+        WebDriverWait(self.driver, 5).until(
+            EC.presence_of_all_elements_located((By.TAG_NAME, "header"))
+        )
+        user_data = dict()
+        header_sections = self.driver.find_elements(By.CSS_SELECTOR, "header > section")
+        
+        user_data['img'] = header_sections[0].find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
+        user_data['username'] = self.keyword
+        try:
+            second_div = header_sections[1].find_element(By.CSS_SELECTOR, 'svg[aria-label="Verified"]')
+            user_data['is_verified'] = True
+        except NoSuchElementException:
+            user_data['is_verified'] = False
+
+        count_list = header_sections[2].find_elements(By.CSS_SELECTOR, "li")
+        user_data['posts'] = count_list[0].find_element(By.CSS_SELECTOR, 'span > span').text
+        user_data['followers'] = count_list[1].find_element(By.CSS_SELECTOR, 'span > span').text
+        user_data['following'] = count_list[2].find_element(By.CSS_SELECTOR, 'span > span').text
+
+
+        user_data['name'] = header_sections[3].find_element(By.CSS_SELECTOR, 'span').text
+        user_data['bio'] = header_sections[3].find_element(By.CSS_SELECTOR, 'h1').text
+        
+        end_dt = datetime.datetime.now()
+        time_diff = end_dt - start_dt
+        self.logger.info(Messages.total_seconds.value.format(time_diff.seconds))
+        
+        return [user_data]
+
+
+
+    def insert_into_mongodb(self, posts, type):
         """Insert Posts into MongoDB"""
         try:
             client = MongoClient(MongoDB.base_url.value)
             data_base = client[MongoDB.client.value]
-            collection = data_base[MongoDB.data.value]
+            collection = data_base[MongoDB.posts.value] if type == 1 else data_base[MongoDB.user.value]
             collection.insert_many(posts)
             client.close()
             self.logger.info(Messages.db_insertion_successfully.value)
@@ -143,16 +213,36 @@ class Scraper:
         try:
             posts = self.search_instagram(1, 9)
             if posts:
-                self.insert_into_mongodb(posts)
+                self.insert_into_mongodb(posts, type=1)
             else:
                 self.logger.info(Messages.no_post_found.value)
+        except Exception as error:
+            self.logger.error(Messages.post_error.value.format(error))
+    
+    def get_user(self):
+        """Get User from server"""
+        self.logger.info(Messages.search.value)
+        try:
+            user = self.search_user()
+            if user:
+                self.insert_into_mongodb(user, type=2)
+            else:
+                self.logger.info(Messages.no_user_found.value)
         except Exception as error:
             self.logger.error(Messages.post_error.value.format(error))
 
 
 if __name__ == "__main__":
     print(Messages.program_title.value)
-    keyword = input(Messages.input.value)
-    instagram_scraper = Scraper(keyword)
-
-    instagram_scraper.get_posts()
+    option = int(input(Messages.option.value))
+    if option == 1:
+        tag = input(Messages.tag.value)
+        instagram_scraper = Scraper(tag)
+        instagram_scraper.get_posts()
+    elif option == 2:
+        username = input(Messages.username.value)
+        instagram_scraper = Scraper(username)
+        instagram_scraper.get_user()
+    else:
+        raise ValueError('Incorrect value')
+    
